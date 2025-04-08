d248e52269e46ab512395eb794133b79e3462162
thanhduongw <levuthanhduong2004@gmail.com>
2025-04-09 02:49:01 +0700

    finish 1. Layout - Grid css

--- lab05/src/App.jsx
+++ lab05/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="custom-container m-2">
+      <div className="header border">
+        HEADER
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="menu border">
+        NAV
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      <div className="content border">
+        OVER VIEW
+      </div>
+
+      <div className="table border">
+        TABLE DATA
+      </div>
+    </div>
+  );
 }
 
 export default App
